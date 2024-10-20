d291318380106016cce8192ddbf9eecfc5e85450
Aayush Chopra <aayushking37@gmail.com>
2024-10-21 01:18:51 +0530

    fixed vercel deployment error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -102,7 +102,7 @@ export default function LoginPage() {
                 </form>
 
                 <p className="mt-4 text-center text-sm text-gray-600">
-                    Don't have an account?{" "}
+                    Don&apos;t have an account?{" "}
                     <a href="/signup" className="text-blue-600 hover:underline">
                         Sign up
                     </a>
